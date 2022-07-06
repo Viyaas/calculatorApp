6ee866deb49ad0db30c66221cf8a0ae22202982d
Viyas <kram78150@gmail.com>
2022-07-06 06:42:34 +0530

    Calculator Application

---
+++ calculator.java
@@ -0,0 +1,12 @@
+public class calculator{
+    public static void main(String args){
+      
+        
+       int  a=500;
+       int  b=50;
+       int result = a+b;
+
+       System.out.println(result);
+
+    }
+}
